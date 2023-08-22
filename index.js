ae7a3d1478cc2762ccd8fe30bc5a7bb6eb510abe
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-08-22 13:14:42 +0200

    Rewrite support and basic debugging

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ async function getParentConfig() {
     delete blocklist.updatedOn;
   }
 
+  // We can't directly set the rewrites for some reason, so we'll have to do that separately
+  delete data.rewrites;
+
   // Delete any arrays that are empty in the parent account's configuration
   for (const key in data) {
     if (Array.isArray(data[key]) && data[key].length === 0) {
@@ -39,6 +42,8 @@ async function getParentConfig() {
       delete data[key];
     }
   }
+
+  if (process.env.DEBUG) console.log(JSON.stringify(data, null, 2));
 
   return data;
 }
@@ -53,6 +58,46 @@ async function setChildConfig(apiKey, config_id, config) {
     },
     body: JSON.stringify(config),
   });
+  if (process.env.DEBUG) console.log(await res.text());
+  return res.ok;
+}
+
+// Function to get the rewrites of an account
+async function getRewrites(apiKey, config_id) {
+  const res = await fetch(`https://api.nextdns.io/profiles/${config_id}/rewrites`, {
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  });
+  const { data } = await res.json();
+  if (process.env.DEBUG) console.log(JSON.stringify(data, null, 2));
+  return data;
+}
+
+// Function to delete a rewrite from an account
+async function deleteRewrite(apiKey, config_id, rewrite_id) {
+  const res = await fetch(`https://api.nextdns.io/profiles/${config_id}/rewrites/${rewrite_id}`, {
+    method: "DELETE",
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  });
+  if (process.env.DEBUG) console.log(await res.text());
+  return res.ok;
+}
+
+// Function to add a rewrite to an account
+// rewrite: { "name": "domain", "content": "ip" }
+async function addRewrite(apiKey, config_id, rewrite) {
+  const res = await fetch(`https://api.nextdns.io/profiles/${config_id}/rewrites`, {
+    method: "POST",
+    headers: {
+      "X-Api-Key": apiKey,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(rewrite),
+  });
+  if (process.env.DEBUG) console.log(await res.text());
   return res.ok;
 }
 
@@ -73,6 +118,36 @@ async function main() {
       console.log(`Configuration set for ${child.name}`);
     } else {
       console.log(`Failed to set configuration for ${child.name}`);
+    }
+  }
+
+  // Get the rewrites from the parent account
+  const rewrites = await getRewrites(config.parent_account_api_key, config.parent_account_config_id);
+
+  // Loop over child accounts
+  for (const child of config.child_accounts) {
+    // Get the rewrites from the child account
+    const childRewrites = await getRewrites(child.api_key, child.config_id);
+
+    // Loop over the rewrites of the child account
+    for (const rewrite of childRewrites) {
+      // Delete the rewrite from the child account
+      const ok = await deleteRewrite(child.api_key, child.config_id, rewrite.id);
+      if (ok) {
+        console.log(`Deleted rewrite ${rewrite.name} from ${child.name}`);
+      } else {
+        console.log(`Failed to delete rewrite ${rewrite.name} from ${child.name}`);
+      }
+    }
+
+    // Loop over the rewrites from the parent account
+    for (const rewrite of rewrites) {
+      delete rewrite.id;
+      delete rewrite.type;
+      if (process.env.DEBUG) console.log(JSON.stringify(rewrite, null, 2));
+      // Add the rewrite to the child account
+      await addRewrite(child.api_key, child.config_id, rewrite);
+      console.log(`Added rewrite ${rewrite.name} to ${child.name}`);
     }
   }
 }
